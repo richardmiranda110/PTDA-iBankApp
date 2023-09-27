4cf283ecfb23d4ef36da26a5d1bec9e6135a1265
Ricardo <93994704+rikkoRicardo@users.noreply.github.com>
2023-09-27 15:57:04 +0100

    preparation for the JSON implementation

---
+++ src/main/java/com/ua/ptda_ibankapp/LoginClient/JSONRegisterTemplate.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ua.ptda_ibankapp.LoginClient;
+
+/**
+ *
+ * @author ricar
+ */
+public class JSONRegisterTemplate {
+
+    private String fullName;
+    private String email;
+    private char[] password;
+
+    public JSONRegisterTemplate(String fullName, String email, char[] password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public char[] getPassword() {
+        return password;
+    }
+}
